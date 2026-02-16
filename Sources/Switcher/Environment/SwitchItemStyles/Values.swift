9415474c4034fecc090a8963bced4d76ad4e6db6
Mahdi Bandegani <mahdibandegani@gmail.com>
2026-02-16 15:44:32 +0330

    feat: add environment values for the keys

---
+++ Sources/Switcher/Environment/SwitchItemStyles/Values.swift
@@ -0,0 +1,50 @@
+//
+//  File.swift
+//  Switcher
+//
+//  Created by Mahdi BND on 2/16/26.
+//
+
+import SwiftUI
+
+public extension EnvironmentValues {
+    var switchItemBackgroundColor: Color {
+        get { self[SwitchItemBackgroundColorKey.self] }
+        set { self[SwitchItemBackgroundColorKey.self] = newValue }
+    }
+
+    var switchItemSelectedBackgroundColor: Color {
+        get { self[SwitchItemSelectedBackgroundColorKey.self] }
+        set { self[SwitchItemSelectedBackgroundColorKey.self] = newValue }
+    }
+
+    var switchItemForegroundColor: Color {
+        get { self[SwitchItemForegroundColorKey.self] }
+        set { self[SwitchItemForegroundColorKey.self] = newValue }
+    }
+
+    var switchItemSelectedForegroundColor: Color {
+        get { self[SwitchItemSelectedForegroundColorKey.self] }
+        set { self[SwitchItemSelectedForegroundColorKey.self] = newValue }
+    }
+
+    var switchItemBorderWidth: CGFloat {
+        get { self[SwitchItemBorderWidthKey.self] }
+        set { self[SwitchItemBorderWidthKey.self] = newValue }
+    }
+
+    var switchItemSelectedBorderWidth: CGFloat {
+        get { self[SwitchItemSelectedBorderWidthKey.self] }
+        set { self[SwitchItemSelectedBorderWidthKey.self] = newValue }
+    }
+
+    var switchItemBorderColor: Color {
+        get { self[SwitchItemBorderColorKey.self] }
+        set { self[SwitchItemBorderColorKey.self] = newValue }
+    }
+
+    var switchItemImageSize: CGFloat {
+        get { self[SwitchItemImageSizeKey.self] }
+        set { self[SwitchItemImageSizeKey.self] = newValue }
+    }
+}
