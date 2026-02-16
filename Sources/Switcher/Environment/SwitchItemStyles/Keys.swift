2b803afd2259deeb1d6cbcbeb934c6d7a6303de5
Mahdi Bandegani <mahdibandegani@gmail.com>
2026-02-16 15:43:55 +0330

    feat: add environment keys to allow item customization

---
+++ Sources/Switcher/Environment/SwitchItemStyles/Keys.swift
@@ -0,0 +1,40 @@
+//
+//  SwitchItemKeys.swift
+//  Switcher
+//
+//  Created by Mahdi BND on 2/16/26.
+//
+
+import SwiftUI
+
+internal struct SwitchItemBackgroundColorKey: EnvironmentKey {
+    static let defaultValue: Color = .clear
+}
+
+internal struct SwitchItemSelectedBackgroundColorKey: EnvironmentKey {
+    static let defaultValue: Color = .accentColor
+}
+
+internal struct SwitchItemForegroundColorKey: EnvironmentKey {
+    static let defaultValue: Color = .secondary
+}
+
+internal struct SwitchItemSelectedForegroundColorKey: EnvironmentKey {
+    static let defaultValue: Color = .primary
+}
+
+internal struct SwitchItemBorderWidthKey: EnvironmentKey {
+    static let defaultValue: CGFloat = 2
+}
+
+internal struct SwitchItemSelectedBorderWidthKey: EnvironmentKey {
+    static let defaultValue: CGFloat = 0
+}
+
+internal struct SwitchItemBorderColorKey: EnvironmentKey {
+    static let defaultValue: Color = .gray
+}
+
+internal struct SwitchItemImageSizeKey: EnvironmentKey {
+    static let defaultValue: CGFloat = 18
+}
