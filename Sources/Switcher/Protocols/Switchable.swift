53ac58b40d7bf0ab7c18e96a049a43383796d5dc
Mahdi Bandegani <mahdibandegani@gmail.com>
2026-02-16 11:59:04 +0330

    feat: add Switchable protocol
    add min platform to package, iOS 15

---
+++ Sources/Switcher/Protocols/Switchable.swift
@@ -0,0 +1,14 @@
+//
+//  Switchable.swift
+//  Switcher
+//
+//  Created by Mahdi BND on 4/27/25.
+//
+
+import SwiftUI
+
+public protocol Switchable: Identifiable, Hashable {
+    var id: Int { get }
+    var switchTitle: String { get }
+    func getImage(selected: Bool, colorScheme: ColorScheme?) -> String?
+}
